4f7215932cbbafc145d76c34fb297c8d77e30ec2
Philipp Saboi <e12112697@student.tuwien.ac.at>
2026-07-25 00:38:52 +0200

    Initial dash setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+      <main className="min-h-screen bg-neutral-950 text-white">
+        <h1 className="text-4xl font-bold">
+          dash.
+        </h1>
+      </main>
+  )
+}
+
+export default App
